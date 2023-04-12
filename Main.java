f6a50108b8ae684964e386b63eed1e2859a5f8df
SzymonRem <126695909+SzymonRem@users.noreply.github.com>
2023-04-12 14:59:35 +0000

    g

--- Main.java
+++ Main.java
@@ -8,18 +8,58 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
-      Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
-
-      var students = s.getStudents();
+      Scanner scan = new Scanner(System.in);
+        System.out.println("Możliwe opcje:");
+      System.out.println("0.Wyjście");
+       System.out.println("1.Dodaj Studenta");
+       System.out.println("2.Wypisz studentów");
+      System.out.println("Wybierz opcje");
+      int opcja =scan.nextInt();
+      
+ Service s = new Service();
+      switch(opcja)
+        {
+          case 1:
+            {
+               System.out.println("Podaj imie");
+               String imie =scan.next();
+              System.out.println("Podaj wiek");
+              int wiek= scan.nextInt();
+                 System.out.println("Podaj nazwisko");
+               String nazwisko =scan.next();
+              
+                s.addStudent(new Student(imie,wiek,nazwisko));
+              break;
+            }
+          case 0:
+            {
+              System.exit(0);
+            }
+          case 2:
+            {
+              var students = s.getStudents();
       for(Student current : students) {
         System.out.println(current.ToString());
       }
+            }
+            
+        }
+      
+      
+      
+     
+     // s.addStudent(new Student("Krzysztof", 20));
+     // s.addStudent(new Student("Janusz", 40));
+
+     // var students = s.getStudents();
+     // for(Student current : students) {
+       // System.out.println(current.ToString());
+     // }
     } catch (IOException e) {
 
     }
